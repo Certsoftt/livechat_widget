9e2246e66f01d1d12baafe3b13800bd00c27e81a
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2025-01-17 02:14:56 +0100

    Add files via upload

---
+++ admin_panel/cache/groupchat1.php
@@ -0,0 +1,16 @@
+<?php
+$groupchat = array();
+$groupchat['id'] = '1';
+$groupchat['password'] = '';
+$groupchat['title'] = 'Weekly Support';
+$groupchat['description'] = '';
+$groupchat['opids'] = '0';
+$groupchat['maxclients'] = 10;
+$groupchat['lang'] = 'en';
+$groupchat['chatstyle'] = 0;
+$groupchat['bgimage'] = 0;
+$groupchat['buttonimg'] = 'colour_on.png';
+$groupchat['floatpopup'] = 0;
+$groupchat['floatcss'] = 'bottom:20px;left:20px';
+$groupchat['active'] = 0;
+?>
